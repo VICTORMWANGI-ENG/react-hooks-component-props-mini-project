cbc5fd4e5fe894ce23bd3f9fbd717dc59ddadd1f
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2024-08-06 22:12:45 +0300

    lab retouch

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,25 @@
+/** @format */
+
 import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header";
 import About from "./About";
-
-
+import ArticleList from "./ArticleList";
 
 console.log(blogData);
 
 function App() {
-  return (
+	return (
 		<div className='App'>
-			<Header />
+			<Header name={blogData.name} />
 			<aside>
-				<About />
+				<About image={blogData.image} about={blogData.about} />
 			</aside>
 			<main>
-		
+				<ArticleList posts={blogData.posts} />
 			</main>
 		</div>
-  );
+	);
 }
 
 export default App;
